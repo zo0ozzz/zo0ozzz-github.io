916c026ee724f659bc40ff5db91bfee9c1366d8d
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-07-15 11:48:45 +0900

    구글드라이브 테스트

--- src/component/posts/Post.jsx
+++ src/component/posts/Post.jsx
@@ -2,12 +2,15 @@ import "./Post.scss";
 import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { HTMLRenderer } from "../common/HTMLRenderer";
+// ckEditor
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export function Post() {
   const { id } = useParams();
   const [post, setPost] = useState({});
   const [mode, setMode] = useState(false);
-  const textarea = useRef(null);
+  const postBodyData = useRef(post.body);
 
   useEffect(() => {
     fetch("http://localhost:9999/data/" + id)
@@ -25,7 +28,8 @@ export function Post() {
             <button
               className="editButton"
               onClick={() => {
-                const bodyValue = textarea.current.value.replace(/^\s+/g, "");
+                const bodyValue = postBodyData.current;
+                // const bodyValue = textarea.current.value.replace(/\t/g, "");
 
                 const data = {
                   body: bodyValue,
@@ -63,18 +67,39 @@ export function Post() {
         <div className="subject">{post.title}</div>
         <div className="content">
           {mode ? (
-            <textarea
-              class="textarea"
-              ref={textarea}
-              defaultValue={post.body}
-              // rows={1}
-              onChange={() => {
-                textarea.current.style.height = "auto";
-                textarea.current.style.height =
-                  textarea.current.scrolHeight + "px";
-              }}
-            />
+            <>
+              <h2>Using CKEditor 5 build in React</h2>
+              <CKEditor
+                editor={ClassicEditor}
+                data={post.body}
+                onReady={(editor) => {
+                  // You can store the "editor" and use when it is needed.
+                  console.log("Editor is ready to use!", editor);
+                }}
+                onChange={(event, editor) => {
+                  postBodyData.current = editor.getData();
+                  // console.log({ event, editor });
+                }}
+                onBlur={(event, editor) => {
+                  // console.log("Blur.", editor);
+                }}
+                onFocus={(event, editor) => {
+                  // console.log("Focus.", editor);
+                }}
+              />
+            </>
           ) : (
+            // <textarea
+            //   class="textarea"
+            //   ref={textarea}
+            //   defaultValue={post.body}
+            //   rows={10}
+            //   onChange={() => {
+            //     textarea.current.style.height = "auto";
+            //     textarea.current.style.height =
+            //       textarea.current.scrollHeight + "px";
+            //   }}
+            // />
             <HTMLRenderer content={post.body} />
           )}
         </div>

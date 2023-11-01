f0e874702433e126d0695d87abce0b3a2b7a5960
zo0ozzz <zo0qzzz@gmail.com>
2023-11-02 03:59:42 +0900

    기기 변경

--- src/component/home/HomeContentList.jsx
+++ src/component/home/HomeContentList.jsx
@@ -6,6 +6,36 @@ import api from "../../lib/axios/axios.js";
 export default function HomeContentList({ sortName }) {
   const [posts, setPosts] = useState([]);
 
+  // // const year = now.getFullYear().toString().slice(-2);
+  // // const month = ("0" + (now.getMonth() + 1).toString()).slice(-2);
+  // // const date = ("0" + now.getDate()).slice(-2);
+  // // // const day = dayArr[now.getDay()];
+  // // const hours = ("0" + now.getHours().toString()).slice(-2);
+  // // const minutes = ("0" + now.getMinutes().toString()).slice(-2);
+  // // const seconds = ("0" + now.getSeconds().toString()).slice(-2);
+
+  // class DateClass {
+  //   constructor() {
+  //     this.now = new Date();
+  //     this.year4 = this.now.getFullYear().toString();
+  //     this.year2 = this.now.getFullYear().toString().slice(-2);
+  //     this.month = ("0" + (this.now.getMonth() + 1).toString()).slice(-2);
+  //     this.date = ("0" + this.now.getDate()).slice(-2);
+  //     this.day = ["일", "월", "화", "수", "목", "금", "토"][this.now.getDay()];
+  //     this.hour = ("0" + this.now.getHours().toString()).slice(-2);
+  //     this.minute = ("0" + this.now.getMinutes().toString()).slice(-2);
+  //     this.second = ("0" + this.now.getSeconds().toString()).slice(-2);
+  //   }
+
+  //   timeCode() {
+  //     return `${this.year2}${this.month}${this.date}${this.hour}${this.minute}${this.second}`;
+  //   }
+  // }
+
+  // const date = new DateClass();
+
+  // console.log(date.timeCode());
+
   const postsList = posts.map((post, index) => {
     function convertDate(dateString) {
       // date는 12자리(년월일시분초) 숫자로 된 문자열
